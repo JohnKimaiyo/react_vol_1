b8af82d025c1e2f6f86f88722551432e8302e35a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-06 18:14:32 +0300

    title props called

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
-import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import Title from "./components/Title";
 function App() {
   return (
     <div className="App">
       <Greet />
       <Welcome />
       <Hello />
+      <Title name="John FrontEnd developer" />
+      <Title name="John Mobile developer" />
+      <Title name="John Blockchain developer" />
     </div>
   );
 }
