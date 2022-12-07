a432c1aae55f484fe14d22f6b2df2336fdee8247
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-07 13:03:47 +0300

    name usetate rendered"

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Title from "./components/Title";
+import MyName from "./components/MyName";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
       <Title name="John FrontEnd developer" />
       <Title name="John Mobile developer" />
       <Title name="John Blockchain developer" />
+      <MyName/>
     </div>
   );
 }
