9723a803a2703c95b8c8022ea099bbcddeb9c3ae
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-10 04:38:17 -0800

    submit event

---
+++ src/components/Submit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Submit() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("You clicked submit.");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
