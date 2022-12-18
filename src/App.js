d2fe709afb85b406ad58d971743c7ef0e83d717e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-18 11:52:03 +0300

    toggle theme event

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeContextProvider from "./context/ThemeContext";
-
+import ThemeToggle from "./components/ThemeToggle";
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle/>
       </ThemeContextProvider>
     </div>
   );
